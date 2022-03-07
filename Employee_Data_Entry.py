032691979a020db03de6f80ef813a963c6d535f5
Kewin.S <Kewinsp300@gmail.com>
2022-03-07 18:18:43 +0000

    some finetuning

--- Employee_Data_Entry.py
+++ Employee_Data_Entry.py
@@ -5,24 +5,24 @@ age_counter = 0
 for employee in range(employees_int):
     
     while True:
-         first_name = input("What is this employees' first name?: ")
+         first_name = input("What is the employees' first name?: ")
          if first_name.isalpha():
             break
     
     while True:
-        last_name = input("What is this employees' last name?: ")
+        last_name = input("What is the employees' last name?: ")
         if last_name.isalpha():
             break
     
     while True: 
-        age_str = input("What is this employees' age?: ")
+        age_str = input("What is the employees' age?: ")
         if age_str.isdigit():
             age = int(age_str)
-            if age >= 18 and age <= 100:
+            if 18 <= age <= 100:
                 age_counter += age
                 break
 
-    print(f"Employee {first_name} {last_name} is {age}")
+    print(f"Employee {first_name} {last_name} is {age} years old")
 
 if age_counter > 500:
     print(f"The total age of the employees is {age_counter}")
